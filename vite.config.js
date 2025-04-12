8b247fdc210254856dc3645564c5293d6c50f03d
Bikash Kumar Choudhury <kumarbikashchoudhury2004@gmail.com>
2025-04-12 16:42:33 +0530

    Change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // ensures correct asset paths on Vercel
+  build: {
+    outDir: 'dist', // default, but good to be explicit
+    emptyOutDir: true, // clears the folder before building
+  },
+});
